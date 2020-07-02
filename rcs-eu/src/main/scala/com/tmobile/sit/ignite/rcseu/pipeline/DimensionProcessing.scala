643002bb2c020efd413f89b156aa325a5a091974
ovidiu.popa <ovidiu.popa@external.t-mobile.cz>
2020-07-02 14:55:10 +0200

    Refactored dimension process into separate class

---
+++ rcs-eu/src/main/scala/com/tmobile/sit/ignite/rcseu/pipeline/DimensionProcessing.scala
@@ -0,0 +1,22 @@
+package com.tmobile.sit.ignite.rcseu.pipeline
+
+import com.tmobile.sit.common.Logger
+import org.apache.spark.sql.DataFrame
+
+trait DimensionProcessing extends Logger{
+  def getUserAgents(activityData: DataFrame, registerRequestsData: DataFrame): DataFrame
+}
+
+class Dimension extends DimensionProcessing {
+
+  override def getUserAgents(activity: DataFrame, registerRequests: DataFrame): DataFrame = {
+    activity
+      .select("user_agent")
+      .union(
+        registerRequests
+          .select("user_agent")
+      )
+      .distinct()
+      .sort("user_agent")
+  }
+}
