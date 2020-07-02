377521e2dd96596acb535ac189de18be9b265979
ovidiu.popa <ovidiu.popa@external.t-mobile.cz>
2020-07-02 14:37:02 +0200

    Adjusted entry point class with updated logic

--- rcs-eu/src/main/scala/com/tmobile/sit/ignite/rcseu/Application.scala
+++ rcs-eu/src/main/scala/com/tmobile/sit/ignite/rcseu/Application.scala
@@ -1,12 +1,9 @@
 package com.tmobile.sit.ignite.rcseu
 
 import com.tmobile.sit.common.Logger
-import com.tmobile.sit.common.readers.{CSVReader, Reader}
+import com.tmobile.sit.common.readers.{CSVReader}
 import com.tmobile.sit.ignite.rcseu.config.Setup
-import com.tmobile.sit.ignite.rcseu.pipeline.{CoreLogicWithTransform, InputData, Pipeline, Stage}
-
-case class Inputs(input1: Reader, input2: Reader, input3: Reader)
-
+import com.tmobile.sit.ignite.rcseu.pipeline.{CoreLogicWithTransform, InputData, Pipeline, ResultPaths, ResultWriter, Stage}
 
 object Application extends App with Logger {
   val conf = new Setup()
@@ -23,16 +20,19 @@ object Application extends App with Logger {
   implicit val sparkSession = getSparkSession(conf.settings.appName.get)
 
   val inputReaders = InputData(
-    activity = new CSVReader(conf.settings.inputPath.get + "activity_*.csv", header = true, delimiter = "\t"),
-    provision = new CSVReader(conf.settings.inputPath.get + "provision_*.csv", header = true, delimiter = "\t"),
-    register_requests = new CSVReader(conf.settings.inputPath.get + "register_request*.csv", header = true, delimiter = "\t")
+    activity = new CSVReader(conf.settings.inputPath.get + "activity_*.gz", header = true, delimiter = "\t"),
+    provision = new CSVReader(conf.settings.inputPath.get + "provision_*.gz", header = true, delimiter = "\t"),
+    register_requests = new CSVReader(conf.settings.inputPath.get + "register_request*.gz", header = true, delimiter = "\t")
   )
 
   val stage = new Stage()
 
   val processingCore = new CoreLogicWithTransform()
 
-  val pipeline = new Pipeline(inputReaders,stage,processingCore, conf.settings)
+  val resultPaths = ResultPaths(conf.settings.lookupPath.get, conf.settings.outputPath.get)
+  val resultWriter = new ResultWriter(resultPaths)
+
+  val pipeline = new Pipeline(inputReaders,stage,processingCore,resultWriter)
 
   pipeline.run()
 
