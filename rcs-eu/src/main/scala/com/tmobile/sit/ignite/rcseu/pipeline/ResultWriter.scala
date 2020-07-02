ee88705b1bd09e610f4284c46599947bcd1f2ff8
ovidiu.popa <ovidiu.popa@external.t-mobile.cz>
2020-07-02 14:04:56 +0200

    Added ResultsWriter class for writing the OutputData object's files

---
+++ rcs-eu/src/main/scala/com/tmobile/sit/ignite/rcseu/pipeline/ResultWriter.scala
@@ -0,0 +1,28 @@
+package com.tmobile.sit.ignite.rcseu.pipeline
+
+import com.tmobile.sit.common.Logger
+import com.tmobile.sit.common.writers.CSVWriter
+import com.tmobile.sit.rbm.data.{FileMetaData, OutputData, ResultPaths}
+import org.apache.spark.sql.SparkSession
+
+trait Writer extends Logger{
+  def write(output: OutputData): Unit
+}
+/**
+ * The ResultWrite class is an implementation of the CSVWriter over a set of output files
+ * required by the RBM pipeline. It takes into consideration the file metadata for the current
+ * file date and natco, as well as the ResultsPath class because it's writing both output and
+ * lookup files for the next iteration
+ */
+class ResultWriter(resultPaths: ResultPaths) (implicit sparkSession: SparkSession) extends Writer {
+
+  override def write(outputData: OutputData) =
+  {
+    logger.info("Writing output files")
+
+    //val fileSuffix = fileMetaData.file_date.replace("-","")+"_"+fileMetaData.file_natco_id
+
+    CSVWriter(outputData.UserAgents, resultPaths.outputPath+"UserAgents.csv", delimiter = ";").writeData()
+
+  }
+}
