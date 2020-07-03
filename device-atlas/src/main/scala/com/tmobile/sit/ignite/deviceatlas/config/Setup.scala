08edc3ab4e279edc8c8493e6828a6156eac47309
lukas.bidmon <lukas.bidmon@external.t-mobile.cz>
2020-07-03 14:47:54 +0200

    Initial commit

---
+++ device-atlas/src/main/scala/com/tmobile/sit/ignite/deviceatlas/config/Setup.scala
@@ -0,0 +1,18 @@
+package com.tmobile.sit.ignite.deviceatlas.config
+
+import com.tmobile.sit.common.config.ServiceConfig
+
+class Setup(configFile: String = "device-atlas.conf")  {
+
+  val settings = {
+    val serviceConf = new ServiceConfig(Some(configFile))
+
+    Settings(
+      appName = Option(serviceConf.envOrElseConfig("configuration.appName.value"))
+      , inputPath = Option(serviceConf.envOrElseConfig("configuration.inputPath.value"))
+      , lookupPath = Option(serviceConf.envOrElseConfig("configuration.lookupPath.value"))
+      , outputPath = Option(serviceConf.envOrElseConfig("configuration.outputPath.value"))
+      , workPath = Option(serviceConf.envOrElseConfig("configuration.workPath.value"))
+     )
+  }
+}
