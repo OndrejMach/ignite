06c3f041bf2a3136650e89d8d7583530bcc208b7
ovidiu.popa <ovidiu.popa@external.t-mobile.cz>
2020-07-30 14:46:21 +0200

    Updated test to read parquet with TacStageReader

--- device-atlas/src/main/scala/com/tmobile/sit/ignite/deviceatlas/debug/FunctionalTests.scala
+++ device-atlas/src/main/scala/com/tmobile/sit/ignite/deviceatlas/debug/FunctionalTests.scala
@@ -2,6 +2,7 @@ package com.tmobile.sit.ignite.deviceatlas.debug
 
 import com.tmobile.sit.common.Logger
 import com.tmobile.sit.common.readers.CSVReader
+import com.tmobile.sit.ignite.common.readers.TacStageReader
 import com.tmobile.sit.ignite.deviceatlas.config.Setup
 import com.tmobile.sit.ignite.deviceatlas.datastructures.FileStructures
 import com.tmobile.sit.ignite.deviceatlas.getSparkSession
@@ -32,5 +33,12 @@ object ParquetTest extends App with Logger {
   logger.info(s"Writing parquet files to stage")
   StageWriter(stageData = cptm_ta_d_tac, partitioned = true,
     path = s"D:\\spark\\workspace\\deviceatlas\\stage\\cptm_ta_d_tac.parquet").writeData()
+
+  val cpt_ta_d_tac_parquet = {
+    logger.info("Reading final Exchange rates")
+    TacStageReader(s"D:\\spark\\workspace\\deviceatlas\\stage\\cptm_ta_d_tac.parquet").read()
+  }
+
+  cpt_ta_d_tac_parquet.show(5)
 
 }
