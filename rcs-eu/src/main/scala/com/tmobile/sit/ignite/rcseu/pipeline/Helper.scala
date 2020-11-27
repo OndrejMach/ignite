441e79a5c8c05c3c964d6f312ba5ee8ce9909937
ovidiu.popa <ovidiu.popa@external.t-mobile.cz>
2020-11-27 21:45:51 +0100

    Implemented helper class

---
+++ rcs-eu/src/main/scala/com/tmobile/sit/ignite/rcseu/pipeline/Helper.scala
@@ -0,0 +1,22 @@
+package com.tmobile.sit.ignite.rcseu.pipeline
+
+import com.tmobile.sit.common.Logger
+import com.tmobile.sit.ignite.rcseu.config.Settings
+import org.apache.spark.sql.SparkSession
+
+
+trait Help extends Logger{
+  def resolvePath(settings: Settings, isHistoric: Boolean): String
+}
+
+class Helper() (implicit sparkSession: SparkSession) extends Help {
+
+  override def resolvePath(settings: Settings, isHistoric: Boolean): String = {
+    // if historic, file is found in the archive
+    // check if it exists
+    // usually fail if it doesn't except for known list
+    // TODO: implement known list
+    val path = if(isHistoric) { settings.archivePath.get} else { settings.inputPath.get }
+    path
+  }
+}
