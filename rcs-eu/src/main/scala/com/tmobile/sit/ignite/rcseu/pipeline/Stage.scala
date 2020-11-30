7f01ca8fa27d1a01ddd63edf72d237d5bf90d43e
ovidiu.popa <ovidiu.popa@external.t-mobile.cz>
2020-11-30 09:29:13 +0100

    Some cleanup

--- rcs-eu/src/main/scala/com/tmobile/sit/ignite/rcseu/pipeline/Stage.scala
+++ rcs-eu/src/main/scala/com/tmobile/sit/ignite/rcseu/pipeline/Stage.scala
@@ -15,79 +15,56 @@ trait StageProcessing extends Logger{
 
 //Creating accumulators for activity data, provision data and register requests data
 class Stage extends StageProcessing {
+
+  // TODO: Decide if we need the natco in these
   override def preprocessActivity(activity: DataFrame,accumulated_activity:DataFrame): DataFrame = {
     //taking today's file (the file with the date from program argument) and adding it to the accumulator
     //eventually replacing the data from the current day processing
     //dropping unused columns
     logger.info("Preprocessing Activity Accumulator")
 
-    //TODO: remove this
-    //logger.info("Daily Activity file")
-    //activity.printSchema()
-
-    //Fixing creation date by substracting one hour from the timestamp
+    // Before changing the input schema to take the creation_date as a string, it was
+    // a timestamp and was causing issues. The fix was:
+    // .withColumn("creation_date", col("creation_date") - expr("INTERVAL 1 HOURS"))
     val dailyFile = activity
       .withColumn("FileDate", lit(date))
-      //.withColumn("creation_date", col("creation_date") - expr("INTERVAL 1 HOURS"))
-      //.withColumn("creation_date", substring(col("creation_date_fixed"), 0, 10))
       .drop("bytes_sent","bytes_received","contribution_id","duration","src_ip","sip_reason")
 
-    // TODO: remove this debug
-
-    logger.info("Daily file schema")
-    dailyFile.printSchema()
-
-    logger.info("Daily file data")
+    /*
+    logger.info("Daily activity source file data:")
     dailyFile
       .withColumn("creation_date", substring(col("creation_date"), 0, 10))
       .groupBy("FileDate", "creation_date")
       .agg(count("creation_date").as("creation_date_count"))
       .orderBy(asc("FileDate"))
-      .show(5)
+      .show(2)
 
-
-    // TODO: remove this debug
-
-    logger.info("Old accumulator data")
-    accumulated_activity
+   logger.info("Old accumulator data")
+   accumulated_activity
       .groupBy("FileDate")
       .agg(count("creation_date").as("creation_date_count"))
       .orderBy(asc("FileDate"))
       .show(5)
-
+    */
 
     logger.info(s"accumulated_activity ${accumulated_activity.filter(col("FileDate") =!= date).count()} daily file: ${dailyFile.count()}")
 
-    logger.info(s"Filtering out old accumulator data for date ${date} and adding daily file")
+    logger.info(s"Filtering out old accumulator data for FileDate ${date} and adding daily file")
 
     val result = accumulated_activity
       .drop("bytes_sent","bytes_received","contribution_id","duration","src_ip","sip_reason")
       .filter(col("FileDate") =!= date)
-      //.withColumn("creation_date", substring(col("creation_date"), 0, 10))
-      //.withColumn("FileDate", col("FileDate").cast("date"))
       .union(dailyFile)
       .orderBy("FileDate")
 
-    logger.info(s"result count: ${result.count()}")
-
-    // TODO: remove this debug
-
-    logger.info("New accumulator data")
-    result
-      .groupBy("FileDate")
-      .agg(count("creation_date").as("creation_date_count"))
-      .orderBy(asc("FileDate"))
-      .show(5)
-
+    logger.info(s"New activity accumulator: ${result.count()}"+" records")
 
     result
   }
 
-
-
+  //TODO: Find out what fields can be dropped to thin the accumulator.'tenant' for sure, we will have separate accumulators per natco
   override def preprocessProvision(provision: DataFrame, accumulated_provision:DataFrame): DataFrame = {
-    //TODO: add logic, similar to RBM
-    logger.info("Preprocessing Provision Accumulator")
+   logger.info("Preprocessing Provision Accumulator")
     val dailyFileProvision = provision
       .withColumn("FileDate", lit(date))
      // .drop("bytes_sent","bytes_received","contribution_id","duration","src_ip","sip_reason")
