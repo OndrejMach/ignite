9496e89abe07c3ff7fd021ed725d3d6b8c5b5fe2
ovidiu.popa <ovidiu.popa@external.t-mobile.cz>
2020-12-02 18:09:33 +0100

    Added file to repo

---
+++ rcs-eu/src/main/scala/com/tmobile/sit/ignite/rcseu/config/RunConfig.scala
@@ -0,0 +1,47 @@
+package com.tmobile.sit.ignite.rcseu.config
+
+// variables needed in FactsProcesing and ProcessingCore for filtering
+class RunConfig(_args: Array[String]) {
+  val args = _args
+
+  // TODO: natco network for Macedonia; move this out
+  val date = args(0)
+  val natco = args(1)
+  val isHistoric = args(2).toBoolean
+
+  val date_split = date.split('-')
+  val (year, monthNum, dayNum) = (date_split(0), date_split(1),date_split(2))
+  val month = year + "-" + monthNum
+
+  val monthforkey = year + "\\" +monthNum
+  val dayforkey =  dayNum +"-"+ monthNum +"-"+ year
+
+  val dateforoutput = year+monthNum+dayNum
+  val monthforoutput = year+monthNum
+
+  val mtID="1"
+  val stID="3"
+  val cgID="2"
+  val crID="4"
+  val mkID="7"
+
+  val natcoID = if (natco == "mt") mtID
+  else if (natco == "st") stID
+  else if (natco == "cr") crID
+  else if (natco == "cg") cgID
+  else if (natco == "mk") mkID
+  else "natco ID is not correct"
+
+  val mt="dt-magyar-telecom"
+  val st="dt-slovak-telecom"
+  val cg="dt-cosmote-greece"
+  val cr="dt-telecom-romania"
+  val mk="-"
+
+  val natcoNetwork = if (natco == "mt") mt
+  else if (natco == "st") st
+  else if (natco == "cr") cr
+  else if (natco == "cg") cg
+  else if (natco == "mk") mk
+  else "natco network is not correct"
+}
