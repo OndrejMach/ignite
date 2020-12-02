574525fa61488cf8af732ba5b1b87d3298c0507c
ovidiu.popa <ovidiu.popa@external.t-mobile.cz>
2020-12-02 11:05:57 +0100

    Refactored variable names

--- rcs-eu/src/main/scala/com/tmobile/sit/ignite/rcseu/data/DataStructures.scala
+++ rcs-eu/src/main/scala/com/tmobile/sit/ignite/rcseu/data/DataStructures.scala
@@ -1,20 +1,22 @@
 package com.tmobile.sit.ignite.rcseu.data
 
-import com.tmobile.sit.common.readers.Reader
 import org.apache.spark.sql.DataFrame
 import org.apache.spark.sql.types.{IntegerType, StringType, StructField, StructType}
 
 case class InputData(activity: DataFrame, provision: DataFrame, register_requests: DataFrame)
 
-case class PreprocessedData( activity:DataFrame,provision: DataFrame, registerRequests: DataFrame)
+case class PersistentData(oldUserAgents: DataFrame,
+                          activity_archives: DataFrame,
+                          provision_archives:DataFrame,
+                          register_requests_archives:DataFrame)
+
+case class PreprocessedData(acc_activity:DataFrame, acc_provision: DataFrame, acc_register_requests: DataFrame)
 
 case class OutputData(AccActivity:DataFrame,AccProvision:DataFrame,AccRegisterRequests:DataFrame,UserAgents: DataFrame,
                       ProvisionedDaily: DataFrame,ProvisionedMonthly:DataFrame,ProvisionedYearly:DataFrame,
                       RegisteredDaily: DataFrame,RegisteredMonthly: DataFrame,RegisteredYearly: DataFrame,
                       ActiveDaily: DataFrame,ActiveMonthly: DataFrame,ActiveYearly: DataFrame,
                       ServiceDaily:DataFrame)
-
-case class PersistentData(oldUserAgents: DataFrame, accumulated_activity: DataFrame,accumulated_provision:DataFrame,accumulated_register_requests:DataFrame)
 
 case class ResultPaths(lookupPath: String, outputPath: String)
 
@@ -57,7 +59,7 @@ object FileSchemas {
     )
   )
 
-  val activityAccSchema = StructType(
+  val accActivitySchema = StructType(
     Seq(
       StructField("creation_date"      ,StringType , false),
       StructField("from_user"          ,StringType , false),
@@ -76,13 +78,14 @@ object FileSchemas {
     )
   )
 
-  val provisionAccSchema = StructType(
+  val accProvidionSchema = StructType(
     Seq(
       StructField("msisdn"      ,StringType , false),
       StructField("FileDate"    ,StringType , false)
     )
   )
-  val registerRequestsAccSchema = StructType(
+
+  val accRegisterRequestsSchema = StructType(
     Seq(
       StructField("msisdn"      ,StringType , false),
       StructField("user_agent"  ,StringType , false),
