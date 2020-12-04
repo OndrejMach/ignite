34fcd792902b186afd636d76d935b047a80c8834
KUCHTOVAA <alexandra.kuchtova@t-mobile.cz>
2020-12-04 17:35:47 +0100

    Register_requests fix

--- rcs-eu/src/main/scala/com/tmobile/sit/ignite/rcseu/pipeline/FactsProcessing.scala
+++ rcs-eu/src/main/scala/com/tmobile/sit/ignite/rcseu/pipeline/FactsProcessing.scala
@@ -53,12 +53,12 @@ class Facts extends FactsProcessing {
     val maxDate = register_requests.groupBy("msisdn").agg(max("FileDate").alias("FileDate"))
     val onlyMaxDate = register_requests.join(maxDate, Seq("msisdn", "FileDate"), "inner")
     val grouped = onlyMaxDate.groupBy("msisdn").agg(collect_set("user_agent").alias("agent_list"))
-    val register_requests_max = grouped.withColumn("maxAgent",getMaxuserAgent(col("agent_list")))
+    val register_requests_max1 = grouped.withColumn("maxAgent",getMaxuserAgent(col("agent_list")))
+    val register_requests_max = register_requests_max1.withColumn("maxAgentLower",lower(col("maxAgent")))
 
     val register_requests_agg =
       register_requests_max
-        .groupBy("maxAgent").agg(countDistinct("msisdn").as("msisdn_count"))
-        //.filter($"maxAgent".isin(testUserAgent:_*))
+        .groupBy((col("maxAgentLower"))).agg(countDistinct("msisdn").as("msisdn_count"),max("maxAgent").as("maxAgent"))
         .orderBy(desc("maxAgent"))
         .withColumnRenamed("maxAgent","UserAgent")
         .withColumnRenamed("msisdn_count","Registered_daily")
